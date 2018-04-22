a67241c75ae8f9ff48c21fe254d297a081f08ae2
didericis <didericis@gmail.com>
2018-04-21 22:07:56 -0400

    Initial commit

---
+++ src/components/HelloWorld/index.jsx
@@ -0,0 +1,45 @@
+import { PureComponent } from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+import { string } from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
+import './hello_world.css';
+
+const generateColor = () => `#${
+  (0x1000000 + ((Math.random()) * 0xffffff))
+    .toString(16)
+    .substr(1, 6)
+}`;
+
+export default class HelloWorld extends PureComponent {
+  static defaultProps = {
+    initialColor: '#000',
+  };
+
+  static propTypes = {
+    initialColor: string,
+  };
+
+  state = {
+    color: this.props.initialColor,
+  };
+
+  componentWillReceiveProps({ initialColor }) {
+    if (initialColor !== this.props.initialColor) {
+      this.setState({ color: initialColor });
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: generateColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div className="component">
+        <h1 style={{ color: this.state.color, padding: '20px' }}>Hello World!</h1>
+        <button onClick={this.handleClick}>Change color</button>
+      </div>
+    );
+  }
+}
+
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { render } from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+const root = document.getElementById('root');
+
+render(<HelloWorld />, root);
+
