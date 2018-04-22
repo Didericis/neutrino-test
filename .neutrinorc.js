78d96dab7c3a4e4ab48f826abdf30fecb5c6859b
didericis <didericis@gmail.com>
2018-04-21 23:13:17 -0400

    Export css without the contenthash

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -2,6 +2,10 @@ module.exports = {
   use: [
     '@neutrinojs/airbnb',
     '@neutrinojs/react-components',
-    '@neutrinojs/mocha'
+    '@neutrinojs/mocha',
+    [
+      '@neutrinojs/style-loader',
+      { extractId: 'extract', extract: { plugin: { filename: '[name].css' } } },
+    ],
   ]
 };
--- src/components/NeutrinoTest/index.jsx
+++ src/components/NeutrinoTest/index.jsx
@@ -1,6 +1,6 @@
 import { PureComponent } from 'react'; // eslint-disable-line import/no-extraneous-dependencies
 import { string } from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
-import './hello_world.css';
+import './index.css';
 
 const generateColor = () => `#${
   (0x1000000 + ((Math.random()) * 0xffffff))
